775e8203614a785527c587fe4af0dda21a409408
Thirulok22 <thirlokm001@gmail.com>
2023-04-01 09:36:36 +0530

    Demo commit by e36

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
 
     public static void main(String[] args) {
         int a=99999900;
-        System.out.println("This was a file for test repo");
+        System.out.println("This was a file for test repo by e36");
     }
 }
